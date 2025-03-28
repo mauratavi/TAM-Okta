09551fb56f5757f13ce16772d5dd56d83e47ed0b
Maura Aviles <maura@Mauras-MacBook-Pro.local>
2025-03-27 22:56:51 -0700

    working changes

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="dev-n3m8z2s3le8ld0ta.us.auth0.com"
+    clientId="dqzucPjmiVKKiRO2Msw3ps8SUE1APTEL"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
